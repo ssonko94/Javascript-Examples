fd5a0583f1c33f25eb7b3bd133227177bf633f82
Ssonko Jimmy <ssonkojimmy@gmail.com>
2020-05-31 23:15:06 +0300

    create a loop for an 8*8 grid.

--- chessBoard.js
+++ chessBoard.js
@@ -17,3 +17,32 @@ When you have a program that generates this pattern,
 define a binding size = 8 and change the program so that it works for any size, 
 outputting a grid of the given width and height.
  */
+let builtString = "";
+const binding_size = 8;
+
+for (let y = 0; y < binding_size; y++) {
+  let space_start = false;
+  if (y % 2 === 0) {
+    space_start = true;
+  }
+
+  for (let x = 0; x < binding_size; x++) {
+    if (x % 2 === 0) {
+      if (space_start === true) {
+        builtString += " ";
+      } else {
+        builtString += "#";
+      }
+    } else {
+      if (space_start === true) {
+        builtString += "#";
+      } else {
+        builtString += " ";
+      }
+    }
+  }
+
+  builtString += "\n";
+}
+
+console.log(builtString);
